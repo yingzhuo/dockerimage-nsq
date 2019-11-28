84b18e6f265ad1ff68266498a3af449f427a85da
yingzhuo <yingzhor@gmail.com>
2019-11-29 06:46:34 +0800

    2019-11-29 06:46:34

--- nsqd/v1.2.0/docker-entrypoint.sh
+++ nsqd/v1.2.0/docker-entrypoint.sh
@@ -4,6 +4,26 @@ set -e
 
 docktool --quiet wait -w="${LOOKUPD_TCP_ADDRESS}"
 
-mkdir -p ${DATA_PATH}
+# --lookupd-tcp-address
+if [[ -z "${LOOKUPD_TCP_ADDRESS}" ]]; then
+    flag_lookupd_tcp_address = ""
+else
+    flag_lookupd_tcp_address = "--lookupd-tcp-address=${LOOKUPD_TCP_ADDRESS}"
+fi
 
-exec nsqd --lookupd-tcp-address=${LOOKUPD_TCP_ADDRESS} --data-path=${DATA_PATH}
+# --data-path
+if [[ -z "${DATA_PATH}" ]]; then
+    flag_data_path = ""
+else
+    mkdir -p ${DATA_PATH}
+    flag_data_path = "--data-path=${DATA_PATH}"
+fi
+
+# --broadcast-address
+if [[ -z "${BROADCAST_ADDRESS}" ]]; then
+    flag_broadcast_address = ""
+else
+    flag_broadcast_address = "--broadcast-address=${BROADCAST_ADDRESS}"
+fi
+
+exec nsqd "$flag_data_path" "$flag_lookupd_tcp_address" "$flag_broadcast_address"
