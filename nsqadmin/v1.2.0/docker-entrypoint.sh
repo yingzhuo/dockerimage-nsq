e2dab128b05a0218cf00ebed202c95be4185855f
yingzhuo <yingzhor@gmail.com>
2019-11-29 14:48:25 +0800

    2019-11-29 14:48:25

--- nsqadmin/v1.2.0/docker-entrypoint.sh
+++ nsqadmin/v1.2.0/docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
-docktool --quiet wait -w="${LOOKUPD_HTTP_ADDRESS}"
+# --lookupd-tcp-address
+# lookupd服务地址,如果没有不需要向服务器注册
+if [[ -z "${LOOKUPD_TCP_ADDRESS}" ]]; then
+    flag_lookupd_tcp_address=""
+else
+    flag_lookupd_tcp_address="--lookupd-tcp-address=${LOOKUPD_TCP_ADDRESS}"
+    docktool --quiet wait -w="${LOOKUPD_TCP_ADDRESS}"       # 等待lookupd启动成功
+fi
 
-exec nsqadmin --lookupd-http-address=${LOOKUPD_HTTP_ADDRESS}
+exec nsqadmin \
+    "$flag_lookupd_tcp_address"
--- nsqlookupd/v1.2.0/docker-entrypoint.sh
+++ nsqlookupd/v1.2.0/docker-entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-exec nsqlookupd
+# --broadcast-address
+# 广播地址,对于lookupd来说,本参数可选
+if [[ -z "${BROADCAST_ADDRESS}" ]]; then
+    flag_broadcast_address=""
+else
+    flag_broadcast_address="--broadcast-address=${BROADCAST_ADDRESS}"
+fi
+
+exec nsqlookupd \
+    "${flag_broadcast_address}"
